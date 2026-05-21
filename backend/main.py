3cffb30896aa27580ed4ef7b8dc5c5e1dd60d653
Hari Venkata <venkatahari961@gmail.com>
2026-05-21 16:20:55 -0400

    Add system metrics monitoring API

--- backend/main.py
+++ backend/main.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI
+import psutil
 
 app = FastAPI()
 
@@ -7,4 +8,13 @@ app = FastAPI()
 def home():
     return {
         "message": "DevOps Monitoring Platform Running"
+    }
+
+
+@app.get("/metrics")
+def get_metrics():
+    return {
+        "cpu_usage": psutil.cpu_percent(),
+        "memory_usage": psutil.virtual_memory().percent,
+        "disk_usage": psutil.disk_usage('/').percent
     }
